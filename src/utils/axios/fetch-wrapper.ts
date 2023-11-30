cfcd47624dcabcd086efa0856c22eef67c76acb7
dickyd9 <dickyd6@gmail.com>
2023-11-30 18:33:08 +0700

    update endpoint

--- src/utils/axios/fetch-wrapper.ts
+++ src/utils/axios/fetch-wrapper.ts
@@ -4,7 +4,7 @@ import router from "@/router"
 import { toast } from "vue3-toastify"
 
 const axiosInstance = axios.create({
-  baseURL: `https://dev-backend.digvation.tech` || `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: `https://dev-backend.digvation.tech/api` || `${import.meta.env.VITE_API_URL}/api`,
   // baseURL: `${import.meta.env.VITE_API_URL}/api`,
   withCredentials: false,
   headers: {
